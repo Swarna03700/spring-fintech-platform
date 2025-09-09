488d20f478f26e79f1ea537e9ca3415a1c57c0a0
Swarna03700 <swarnadeepghosh441@gmail.com>
2025-09-09 18:25:25 +0530

    Initial commit: Add Gradle project structure

---
+++ services/project-01-wallet/src/main/java/com/fintech/wallet/WalletApp.java
@@ -0,0 +1,9 @@
+package com.fintech.wallet;
+
+import org.springframework.boot.SpringApplication;
+
+public class WalletApp {
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApp.class, args);
+	}
+}
---
+++ services/project-01-wallet/src/test/java/com/fintech/wallet/WalletAppTests.java
@@ -0,0 +1,13 @@
+package com.fintech.wallet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class WalletAppTests {
+	
+	@Test
+	void aSimpleAssertion() {
+		assertEquals(2, 2);
+	}
+}
